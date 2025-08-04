026d82c17f58c85456677e5094517d18d0b394f2
Chanelle <chanelletearikiaua@gmail.com>
2025-08-04 13:28:13 +1000

    Rough layout of code - need to fix quite a few errors between files

--- hydration.py
+++ hydration.py
@@ -4,3 +4,48 @@ from tabulate import tabulate
 
 init(autoreset=True)
 
+class entry: 
+    def __init__(self, date, amount):
+        self.date = date
+        self.amount = amount
+        
+class tracker: 
+    def __init__(self, goal_ml=2000):
+        self.entries = []
+        self.goal_ml = goal_ml
+        
+    def set_goat(self, new_goal): 
+        self.goal_ml = new_goal
+        
+    def add_entry(self, amount):
+        today = datetime.now().strftime("%Y-%m-%d")
+        self.entries.append(entry(today, amount))
+        
+    def total_today(self):
+        today = datetime.now().strftime("%Y-%m-%d")
+        return sum(e.amount for e in self.entries if e.date == today)
+    
+    def save_to_file(self, filename="hydration_log.txt"):
+        with open(filename, 'w') as f:
+            f.write(f'GOAL,{self.goal_ml}\n')
+            for e in self.entries:
+                f.write(f'{e.date},{e.amount}\n')
+                
+    def load_from_file(self, filename="hydration_log.txt"):
+        try:
+            with open(filename, 'r') as f:
+                self.entries = []
+                for line in f:
+                    if line.startswith("GOAL"):
+                        _, goal = line.strip().split(',')
+                        self.goal_ml = int(goal)
+                    else:
+                        date, amount = line.strip().split(',')
+                        self.entries.append(entry(date, int(amount)))
+        except FileNotFoundError: #for file not found
+            print(Fore.RED + 'No history found!')
+            
+    def view_history(self):
+        table = [[e.date, f'{e.amount} ml'] for e in self.entries]
+        print(tabulate(table, headers = ['Date', 'Amount'], tablefmt='fancy_grid'))
+
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from pyfiglet import figlet_format
 from colorama import init, Fore, Style
 from auth import login_user, register_user
+from hydration import tracker
 from emoji import emojize
 
-init(autoreset = True)
+init(autoreset = True) #reset for colorama .style
+def main():
+    tracker = tracker()
+    tracker.load_from_file()
 
 print(Fore.LIGHTBLUE_EX + figlet_format('Welcome to H20 Pal!'))
 
 user = None
-while not user:
+while not user: #main menu for login/register/exit program
     print('\n[1] Login')
     print('[2] Register')
     print('[3] Exit')
@@ -23,17 +27,55 @@ while not user:
     else:
         print(Fore.RED + 'Invalid option. Try again!')
 if user:
-    print(Fore.GREEN + f'Hello, {user}!')
+    print(Fore.GREEN + f'Hello, {user}!') #welcomes user
 
-'''while True:
-    print(Fore.BLUE + '\nWhat would you like to do today?')
+while True:
+    print(Fore.BLUE + '\nWhat would you like to do today?') #main options once logged in
     print('[1] Log water intake')
     print('[2] Set daily goal')
     print('[3] Today\'s total intake')
-    print('[4] View history')
-    print('[5] Logout')
+    print('[4] Save')
+    print('[5] Load')
+    print('[6] View history')
+    print('[7] Logout')
     print('[6] Exit')
     
-    try:
-        choice = int(input('Please enter a menu number: '))'''
+    choice = (input('Please enter a menu number: '))
+    
+    if choice == '1':
+        try:
+            amount = int(input("Enter amount in ml: "))
+            tracker.add_entry(amount)
+            print(Fore.GREEN + f'Successfully added {amount}mls!')
+        except ValueError:
+            print(Fore.RED + 'Unsuccessful! Please add a valid number.')
+    elif choice == '2':
+        try:
+            new_goal = int(input('Enter new daily hydration goal in ml: '))
+            tracker.set_goal(new_goal)
+            print(Fore.CYAN + 'Goal set to {new_goal}ml')
+        except ValueError:
+            print(Fore.RED + 'Please enter a valid number!')
+    elif choice == '3':
+        total = tracker.total_today()
+        print(f'\nTotal today: {total} mls')
+    elif choice == '4':
+        tracker.save_to_file
+    elif choice == '5':
+        tracker.load_from_file
+    elif choice == '6':
+        tracker.view_history()
+    elif choice == '7':
+        print(Fore.YELLOW + f'Successfully logging out {user}...')
+        break
+    elif choice == '8':
+        print(emojize(Fore.BLUE + '\nStay hydrated! :droplet:'))
+    else:
+        print(Fore.RED + 'Invalid choice. Please select a valid option from the menu!')
+        
+if __name__ == '__main__':
+    main()
+        
+            
+            
         
